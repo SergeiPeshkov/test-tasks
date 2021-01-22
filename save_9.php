7e94d288d9bebd600613a5c4c8c9b2bf7250376f
SP <peshkovsergei@gmail.com>
2021-01-22 17:00:12 +0300

    10 tasks complete

--- save_9.php
+++ save_9.php
@@ -1,11 +1,11 @@
 <?php
     $text = $_POST["text"];                                
-    $pdo=new PDO ("mysql:host=localhost; dbname=my_project", "mysql", "mysql" );
+    $pdo=new PDO ("mysql:host=localhost; dbname=my_project", "root", "root" );
     $sql= "INSERT INTO my_table (text) VALUES (:text)";
     $statement = $pdo->prepare($sql);
     $statement->execute(['text' => $text]);
   
-    header ("location: /task_9.php");
+    header ("location: /task_10.php");
 
   ?>
 
--- task_9.php
+++ task_9.php
@@ -35,7 +35,7 @@
                         <div class="panel-content">
                             <div class="panel-content">
                                 <div class="form-group">
-                                    <form action="save_9.php" method="post">
+                                    <form action="save_10.php" method="post">
                                         <label class="form-label" for="simpleinput">Text</label>
                                         <input type="text" id="simpleinput" class="form-control" name="text">
                                         <button class="btn btn-success mt-3" type="submit">Submit</button>
